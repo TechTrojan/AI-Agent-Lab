3311300161d7327ef7530ea43ac2c4b4a36a435e
Mukesh Zala <13876118+TechTrojan@users.noreply.github.com>
2026-02-15 09:54:51 -0600

    Add Windows native dependency repair for rollup/esbuild

---
+++ src/data/profile.ts
@@ -0,0 +1,6 @@
+export const profile = {
+  name: 'Mukesh Zala',
+  github: 'https://github.com/mukeshzala',
+  linkedin: 'https://www.linkedin.com/in/mukeshzala/',
+  email: 'contact@mukeshzala.dev'
+};
